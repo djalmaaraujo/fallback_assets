b343ab3a6e6b15604fca7c813e15b67828dafd83
djalmaaraujo <djalma.araujo@gmail.com>
2014-01-02 19:26:27 -0300

    removing external modules specs

--- spec/lib/fallback_assets/fallback_assets_spec.rb
+++ spec/lib/fallback_assets/fallback_assets_spec.rb
@@ -11,40 +11,6 @@ describe FallbackAssets do
     stub_const "RAILS_ENV", "development"
   end
 
-  describe "FallbackAssets::JavascriptsAssets" do
-    describe "#javascript_include_fallback" do
-
-      it "returns a for production environment" do
-        stub_const "RAILS_ENV", "production"
-        expect(FallbackAssets::JavascriptsAssets.javascript_include_fallback("a", "b")).to eq "a"
-      end
-
-      it "returns b for environments different from production" do
-        stub_const "RAILS_ENV", "development"
-        expect(FallbackAssets::JavascriptsAssets.javascript_include_fallback("a", "b")).to eq "b"
-      end
-
-    end
-
-  end
-
-  describe "FallbackAssets::StylesheetsAssets" do
-    describe "#stylesheet_include_fallback" do
-
-      it "returns a for production environment" do
-        stub_const "RAILS_ENV", "production"
-        expect(FallbackAssets::StylesheetsAssets.stylesheet_include_fallback("a", "b")).to eq "a"
-      end
-
-      it "returns b for environments different from production" do
-        stub_const "RAILS_ENV", "development"
-        expect(FallbackAssets::StylesheetsAssets.stylesheet_include_fallback("a", "b")).to eq "b"
-      end
-
-    end
-
-  end
-
   describe "Configuration File" do
     before do
       YAML.stub(:load_file).with("#{Rails.root}/config/fallback_assets.yml").and_return(settings)
