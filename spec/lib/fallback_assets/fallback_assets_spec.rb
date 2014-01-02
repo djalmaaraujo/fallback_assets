5edb020f53ac48a25f15bb1a00f4cca5241e70e9
djalmaaraujo <djalma.araujo@gmail.com>
2014-01-02 15:49:31 -0300

    version specs

--- spec/lib/fallback_assets/fallback_assets_spec.rb
+++ spec/lib/fallback_assets/fallback_assets_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe FallbackAssets do
   specify { FallbackAssets.should be_a(Module) }
+
+  describe "Lib Version" do
+
+    it "must have a valid version" do
+      FallbackAssets::VERSION.should_not be_nil
+      FallbackAssets::VERSION.should be_a_kind_of String
+    end
+
+  end
+
 end
